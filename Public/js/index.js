a4166168cb6bb2923e67ca2912325c51822924e7
iamsss <saurav9760@gmail.com>
2018-06-09 22:35:15 +0530

    Adding Basics Bulb On And Off Functions

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,1 +1,10 @@
 console.log('Hi Working');
+
+function buttonOff() {
+    document.getElementById('myImage').src='/images/pic_bulboff.gif';
+}
+
+function buttonOn() {
+    document.getElementById('myImage').src='/images/pic_bulbon.gif';
+
+}
