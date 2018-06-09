109029cc75ed5108784df576c32ba76fd5222ef0
iamsss <saurav9760@gmail.com>
2018-06-10 00:08:46 +0530

    Adding Put And Get Method Of Bulb Controller

--- Server/Controller/BulbController/bulb.controller.js
+++ Server/Controller/BulbController/bulb.controller.js
@@ -11,3 +11,30 @@ app.post('/bulbs',(req,res) => {
         res.status(404).send(e);
     });
  });
+
+ app.put('/bulbs',(req,res) => {
+    var body = _.pick(req.body,['name','status']);
+    Bulb.findOneAndUpdate({name: body.name}, 
+    { $set: {
+        status: body.status
+        }
+    },
+    {
+        returnOriginal: false
+    }
+    
+    ).then((result) => {
+        res.send("Sucess");
+    }).catch((e) => {
+        res.status(404).send(e);
+    });
+ });
+
+
+ app.get('/bulbs/status',(req,res) => {
+        Bulb.find({name:"b1"}).then((obj) => {
+            res.send(obj[0].status);
+ }).catch((e) => {
+    res.status(404).send(e);
+});
+});
